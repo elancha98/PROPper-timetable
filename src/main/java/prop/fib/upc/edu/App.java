a4bd26901fafc49022a0fca5ad2efb1d65cc89a2
Ernesto Lanchares <e.lancha98@gmail.com>
2018-10-20 00:24:14 +0200

    Initial commit

---
+++ src/main/java/prop/fib/upc/edu/App.java
@@ -0,0 +1,12 @@
+package prop.fib.upc.edu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/prop/fib/upc/edu/AppTest.java
@@ -0,0 +1,11 @@
+package prop.fib.upc.edu;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    public void testMain() {
+        assert true;
+    }
+}
